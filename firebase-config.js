27f347f1be5266466db9cf4d82c7ec1998465665
99eliel <131190282+99eliel@users.noreply.github.com>
2026-09-01 18:12:09 -0300

    Configura Firebase do PWA

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCkx2ZVsgLXUovuVDpIpHcs6Sm7lt4IYFw",
+  authDomain: "cotacao-78d9f.firebaseapp.com",
+  projectId: "cotacao-78d9f",
+  storageBucket: "cotacao-78d9f.firebasestorage.app",
+  messagingSenderId: "338296297774",
+  appId: "1:338296297774:web:6eea8d574e79ded6ac42bf",
+  measurementId: "G-PF9RQ4XWJT"
+};
