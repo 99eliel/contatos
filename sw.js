d19af27379f06e1718fc81851d7997b92bf35856
99eliel <131190282+99eliel@users.noreply.github.com>
2026-09-01 18:15:05 -0300

    Atualiza cache do PWA para versao Firebase

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='contatos-pwa-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='contatos-pwa-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./firebase-config.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{
@@ -8,5 +8,7 @@ self.addEventListener('fetch',event=>{
     event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response;}).catch(()=>caches.match('./index.html')));
     return;
   }
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
   event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));
 });
