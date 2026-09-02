01a05da4e3eb27428101f2d96bb7ad9e4f9037f4
99eliel <131190282+99eliel@users.noreply.github.com>
2026-09-02 06:53:08 -0300

    Atualiza cache do PWA para identidade administrativa v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='contatos-pwa-v4';
+const CACHE='contatos-pwa-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./firebase-config.js'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
